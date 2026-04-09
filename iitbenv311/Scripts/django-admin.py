3805bb526246926192b634cc3c8026410f31ebe9
SOHAM7110 <sohamkandalkar7110@gmail.com>
2026-04-09 20:21:23 +0530

    Initial Project upload

---
+++ iitbenv311/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\SOHAM\Desktop\Summer Felloswship\workshop_booking\iitbenv311\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
